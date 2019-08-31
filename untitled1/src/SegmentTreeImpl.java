b8f1179c24060d2a95b7c064f3770edfa365ecaf
ujwalgussain <ujwalgussain@gmail.com>
2019-08-31 22:23:09 +0530

    changes

--- untitled1/src/SegmentTreeImpl.java
+++ untitled1/src/SegmentTreeImpl.java
@@ -51,6 +51,26 @@ class SegmentTree
         segmentTree[node]+=diff;
         return diff;
     }
+    private int getSumUtil(int qs, int qe, int ss, int se, int currentNode)
+    {
+      //  System.out.println(String.format("ss = %s se = %s currentNode = %s", ss,se,currentNode));
+        if(qs<=ss && qe>=se){
+         //   System.out.println("Return " + segmentTree[currentNode]);
+            return segmentTree[currentNode];
+        }
+       /* if (qs>se || qe<ss) */
+        if (se < qs || ss > qe) {
+            //System.out.println("Return 0");
+            return 0;
+        }
+        int mid = (ss+se)/2;
+        return getSumUtil(qs,qe, ss, mid, currentNode*2)+
+                getSumUtil(qs,qe, mid+1, se, currentNode*2 +1);
+    }
+    int findSumForRange(int qs, int qe)
+    {
+        return getSumUtil(qs,qe,0,arr.length-1,1);
+    }
 }
 public class SegmentTreeImpl {
     static public void main(String... args)
@@ -58,8 +78,11 @@ public class SegmentTreeImpl {
         int[] arr = new int[]{1,2,3,4,5,6};
         SegmentTree s = new SegmentTree(arr);
         System.out.println("Seg Tree -> " + Arrays.toString(s.getSegmentTree()));
-        s.updateQuery(1,10,1,0,arr.length-1);
-        System.out.println("Seg Tree -> " + Arrays.toString(s.getSegmentTree()));
+      //  s.updateQuery(1,10,1,0,arr.length-1);
+       // System.out.println("Seg Tree -> " + Arrays.toString(s.getSegmentTree()));
+       // System.out.println("Sum of 1-3 " + s.findSumForRange(1,1));
+        System.out.println("Sum of 0-5 " + s.findSumForRange(5,5));
+        System.out.println("Sum of 0-5 " + s.findSumForRange(1,5));
 
     }
 }
